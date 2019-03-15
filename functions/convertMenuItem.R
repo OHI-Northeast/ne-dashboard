ca13f179bcc3210dbbc5ae380a41c0653d28931d
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2019-03-15 10:44:24 -0700

    init commit. this allows sub-goals as drop down tabs in menu

---
+++ functions/convertMenuItem.R
@@ -0,0 +1,8 @@
+convertMenuItem <- function(mi,tabName) {
+  mi$children[[1]]$attribs['data-toggle']="tab"
+  mi$children[[1]]$attribs['data-value'] = tabName
+  if(length(mi$attribs$class)>0 && mi$attribs$class=="treeview"){
+    mi$attribs$class=NULL
+  }
+  mi
+}
